6135d06f56ecb32cbb7c6b1fe0159aa6f9f966fa
Costavin <costantinovinc@gmail.com>
2026-02-08 17:22:56 +0300

    Added null object pattern

---
+++ Behavioral_Patterns/NullObject/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+class Car {
+	public:
+		virtual void drive() const {}
+		virtual void stop() const {}
+};
+
+class Mercedes: public Car {
+	public:
+		void drive() const override {
+			std::cout << "mercedes is moving" << std::endl;
+		}
+		void stop() const override {
+			std::cout << "mercedes is still" << std::endl;
+		}
+};
+
+class Ferrari: public Car {
+	public:
+		void drive() const override {
+			std::cout << "ferrari is moving" << std::endl;
+		}
+		void stop() const override {
+			std::cout << "ferrari is still" << std::endl;
+		}
+};
+
+class NullCar: public Car {
+	public:
+		void drive() const override {}
+		void stop() const override {}
+};
+
+class Rental {
+	private:
+		Car& car;
+	public:
+		Rental(Car& car): car(car) {}
+		void testCar() {
+			car.drive();
+			car.stop();
+		}
+};
+
+int main() {
+	Mercedes merc;
+	Ferrari fer;
+	NullCar nc;
+
+	Rental r1(merc);
+	Rental r2(fer);
+	Rental r3(nc);
+//	Rental* r1 = new Rental(merc);
+//	Rental* r2 = new Rental(fer);
+//	Rental* r3 = new Rental(nc);
+
+	r1.testCar();
+	r2.testCar();
+	r3.testCar();
+
+	return 0;
+}
